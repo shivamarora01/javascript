3db665963de7e42fa96d513d120b374294a240ed
shivamarora01 <85702823+shivamarora01@users.noreply.github.com>
2024-01-03 08:01:09 +0000

    hoisting definations

--- 03_hoisting/01_hoisting.js
+++ 03_hoisting/01_hoisting.js
@@ -1,3 +1,8 @@
+//Hoisting is phenomenon in javascript in which you can use variables 
+//and function even without prior 
+//declaring them
+
+
 var x = 7;
 function getname() {
     console.log("Hie Sieey")
